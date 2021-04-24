cc7c524afcb4541052fdd61bb9c277265dda36d3
Andrey <kronosdidi@gmail.com>
2021-04-24 18:49:40 +0300

    complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Введите ключ");
+        String key = sc.nextLine();
+        System.out.println("Введите сообщение");
+        String message = sc.nextLine();
+        message = encode(message, key);
+        System.out.println(message);
+        System.out.println(decode(message, key));
     }
+
+    public static String encode(String message, String key) {
+        String result = "";
+        for (int i = 0; i < message.length(); i++) {
+            // Складываем по модулю 2
+            int c = (key.charAt(i % key.length()) ^ message.charAt(i));
+            result += Character.toString(c);
+        }
+        return result;
+    }
+
+    public static String decode(String message, String key) {
+        String result = "";
+        for (int i = 0; i < message.length(); i++) {
+            // Складываем по модулю 2
+            int c = (key.charAt(i % key.length()) ^ message.charAt(i));
+            result += Character.toString(c);
+        }
+        return result;
+    }
+
 }
